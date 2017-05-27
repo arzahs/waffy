dece06222dbbd3c1bf67cbb076c8cf9a46eeda35
Dan Enman <enmand@gmail.com>
2017-05-27 19:56:22 -0300

    Separate proto services into separate directory packages

--- build.go
+++ build.go
@@ -1,9 +1,10 @@
 //
 // Generate Protobuf services and messages
 //
-//go:generate protoc --proto_path=.:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/certificates.proto
-//go:generate protoc --proto_path=.:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/users.proto
-//go:generate protoc --proto_path=.:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/sites.proto
-//go:generate protoc --proto_path=.:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/rules.proto
+//go:generate protoc --proto_path=.:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/certificates/certificates.proto
+//go:generate protoc --proto_path=.:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/nodes/nodes.proto
+//go:generate protoc --proto_path=.:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/users/users.proto
+//go:generate protoc --proto_path=.:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/sites/sites.proto
+//go:generate protoc --proto_path=.:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/rules/rules.proto
 
 package waffy
