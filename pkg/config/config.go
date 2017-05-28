aa04c948fb5291126c2ca2f90a1bd31d6687a751
Dan Enman <enmand@gmail.com>
2017-05-27 21:15:27 -0300

    Add CertPath and RPCName on Config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -11,12 +11,24 @@ import (
 const (
 	// DefaultWAFListen is the default
 	DefaultAPIListen = "0.0.0.0:8500"
+
+	// DefaultCertPath is the default path to certificates
+	DefaultCertPath = "./etc"
+
+	// DefaultRPCName is the hostname of the RPC
+	DefaultRPCName = "waffy.local"
 )
 
 // Config is the main configuration for waffy
 type Config struct {
 	// API Listener is the address the API should listen on
 	APIListen string
+
+	// CertPath is the path to certificates for the system
+	CertPath string
+
+	// RPCName is the hostname of the RPC client
+	RPCName string
 }
 
 var cfg *Config
@@ -27,14 +39,23 @@ func init() {
 		panic("cannot read configuration environment")
 	}
 
-	apiListen := os.Getenv("HERMES_API_LISTEN")
-	if apiListen == "" {
-		apiListen = DefaultAPIListen
+	cfg = &Config{
+		APIListen: getEnv("WAFFY_API_LISTEN", c, DefaultAPIListen),
+		CertPath:  getEnv("WAFFY_CERT_PATH", c, DefaultCertPath),
+		RPCName:   getEnv("WAFFY_RPC_NAME", c, DefaultRPCName),
+	}
+}
+
+func getEnv(cfg string, c map[string]string, defValue string) string {
+	val := os.Getenv(cfg)
+	if val == "" {
+		var ok bool
+		if val, ok = c[cfg]; !ok {
+			val = defValue
+		}
 	}
 
-	cfg = &Config{
-		APIListen: apiListen,
-	}
+	return val
 }
 
 // Load returns the loaded configuration
