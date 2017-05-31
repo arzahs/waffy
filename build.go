caeb26990e94d482d2895f541ca347f92330d827
Dan Enman <enmand@gmail.com>
2017-05-30 23:16:39 -0300

    Restructure RPC ProtoBuf messages and services to be in pkg/services

--- build.go
+++ build.go
@@ -1,10 +1,10 @@
 //
 // Generate Protobuf services and messages
 //
-//go:generate protoc --proto_path=.:../../../:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/certificates/certificates.proto
-//go:generate protoc --proto_path=.:../../../:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/nodes/nodes.proto
-//go:generate protoc --proto_path=.:../../../:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/users/users.proto
-//go:generate protoc --proto_path=.:../../../:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/sites/sites.proto
-//go:generate protoc --proto_path=.:../../../:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./protos:. --gofast_out=plugins=grpc:./services/ ./protos/rules/rules.proto
+//go:generate protoc --proto_path=.:../../../:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./pkg/services/protos:. --gofast_out=plugins=grpc:./ ./pkg/services/protos/certificates/certificates.proto
+//go:generate protoc --proto_path=.:../../../:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./pkg/services/protos:. --gofast_out=plugins=grpc:./ ./pkg/services/protos/nodes/nodes.proto
+//go:generate protoc --proto_path=.:../../../:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./pkg/services/protos:. --gofast_out=plugins=grpc:./ ./pkg/services/protos/users/users.proto
+//go:generate protoc --proto_path=.:../../../:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./pkg/services/protos:. --gofast_out=plugins=grpc:./ ./pkg/services/protos/sites/sites.proto
+//go:generate protoc --proto_path=.:../../../:./vendor:./vendor/github.com/gogo/protobuf/protobuf:./pkg/services/protos:. --gofast_out=plugins=grpc:./ ./pkg/services/protos/rules/rules.proto
 
 package waffy
