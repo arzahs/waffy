af40aab9506af12ac472f6995da1df48d7fd8d09
Dan Enman <enmand@gmail.com>
2017-05-31 18:30:36 -0300

    Add CreateCertificate repository

--- pkg/repository/certificates.go
+++ pkg/repository/certificates.go
@@ -1,1 +1,21 @@
 package repository
+
+import (
+	"github.com/unerror/waffy/pkg/data"
+	"github.com/unerror/waffy/pkg/services/protos/certificates"
+)
+
+const (
+	// CertificateBucket is the Store Bucket that cerificates are stored in
+	CertificateBucket = "certificates"
+)
+
+// CreateCertificate creates a Certificate in the data store
+func CreateCertificate(d data.Bucket, c *certificates.Certificate) error {
+	b, err := d.Bucket(CertificateBucket)
+	if err != nil {
+		return err
+	}
+
+	return Create(b, []byte(c.SerialNumber), c)
+}
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -7,6 +7,7 @@ import (
 	"github.com/unerror/waffy/pkg/data"
 )
 
+// Create will create a generic Marshable message m with key k in data.Store b
 func Create(b data.Store, k []byte, m proto.Marshaler) error {
 	if _, err := b.Get(k); err == nil {
 		return fmt.Errorf("%s already exists", k)
