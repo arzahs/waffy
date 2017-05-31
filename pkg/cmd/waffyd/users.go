7491b7af0235131c45ea8a222431cf9d545cfc29
Dan Enman <enmand@gmail.com>
2017-05-31 18:31:34 -0300

    Implement user bootstrapping from the leader

--- pkg/cmd/waffyd/users.go
+++ pkg/cmd/waffyd/users.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"strconv"
 
+	"crypto/rsa"
+
 	"github.com/unerror/waffy/pkg/config"
+	"github.com/unerror/waffy/pkg/crypto"
 	"github.com/unerror/waffy/pkg/data"
 	"github.com/unerror/waffy/pkg/repository"
+	"github.com/unerror/waffy/pkg/services/protos/certificates"
 	"github.com/unerror/waffy/pkg/services/protos/users"
 	"gopkg.in/urfave/cli.v1"
 )
@@ -20,7 +24,7 @@ func init() {
 			{
 				Name:  "create",
 				Usage: "Create a new root user",
-				Flags: []cli.Flag{
+				Flags: append(certificateFlags,
 					cli.StringFlag{
 						Name:  "full-name",
 						Usage: "The user's full name",
@@ -33,7 +37,7 @@ func init() {
 						Name:  "role",
 						Usage: "The user's role",
 					},
-				},
+				),
 				Action: createUser,
 			},
 		},
@@ -61,24 +65,63 @@ func createUser(ctx *cli.Context) error {
 		return err
 	}
 
-	roleId, err := strconv.Atoi(roleStr)
-	if err != nil {
-		return fmt.Errorf("unknown role: %s", roleStr)
-	}
-	switch roleId {
-	case 0:
-		role = users.Role_USER
-	case 1:
-		role = users.Role_ADMIN
-	default:
-		return fmt.Errorf("unknown role ID: %d", roleId)
+	write := ctx.Bool("overwrite")
+	// check if the user already exists
+	if _, err := repository.FindUserByEmail(db, email); err != nil {
+		write = true
 	}
 
-	u := users.User{
-		Name:  fullName,
-		Email: email,
-		Role:  role,
+	// create the user if not (or we want to overwrite)
+	if write {
+		roleId, err := strconv.Atoi(roleStr)
+		if err != nil {
+			return fmt.Errorf("unknown role: %s", roleStr)
+		}
+		switch roleId {
+		case 0:
+			role = users.Role_USER
+		case 1:
+			role = users.Role_ADMIN
+		default:
+			return fmt.Errorf("unknown role ID: %d", roleId)
+		}
+
+		u := users.User{
+			Name:  fullName,
+			Email: email,
+			Role:  role,
+		}
+
+		ca, caKey, err := config.LoadCA()
+
+		key, err := crypto.NewPrivateKey(ctx.Int("key-size"))
+		if err != nil {
+			return err
+		}
+
+		cert, err := crypto.NewCertificate(ca, caKey, key, false, u.Email)
+		u.Certificate = &certificates.Certificate{
+			Subject: &certificates.Subject{
+				Email: u.Email,
+			},
+			SerialNumber: cert.SerialNumber.Bytes(),
+			Certificate:  crypto.EncodePEM(cert),
+		}
+
+		err = repository.CreateCertificate(db, u.Certificate)
+		if err != nil {
+			return err
+		}
+
+		err = config.SaveClientCert(u.Email, cert, key.(*rsa.PrivateKey))
+		if err != nil {
+			return err
+		}
+
+		return repository.CreateUser(db, &u)
+	} else {
+		fmt.Print("Unable to create user %s since they already exist. --overwrite to force\n")
 	}
 
-	return repository.CreateUser(db, &u)
+	return nil
 }
