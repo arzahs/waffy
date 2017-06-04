386dc4bdf0f946bbe5750a0fbe900d827c3854e6
Dan Enman <enmand@gmail.com>
2017-06-03 22:55:27 -0300

    Add Snapshot/Persist tests, multi-peer raft

--- pkg/data/store_raft_test.go
+++ pkg/data/store_raft_test.go
@@ -13,8 +13,8 @@ func TestRaftStore(t *testing.T) {
 	tmpDir, _ := ioutil.TempDir("", "raft_test")
 	defer os.Remove(tmpDir)
 
-	s, _ := NewDB(fmt.Sprintf("%s/raft_test.db", tmpDir))
-	d, err := NewRaft(tmpDir, "127.0.0.1:0", s)
+	d, s, tmpDir, err := mustRaft("raft_test")
+	defer os.RemoveAll(tmpDir)
 
 	Convey("Opening a bad Raft listen address should fail", t, func() {
 		_, err := NewRaft(tmpDir, "256.0.0.0:3453", s)
@@ -130,7 +130,89 @@ func TestRaftStore(t *testing.T) {
 
 		underlyingBucket, _ := d.(*Raft).bucket("/root/")
 		underNodes, _ := underlyingBucket.List()
-
-		So(nodes, ShouldResemble, underNodes)
+		So(underNodes, ShouldContain, Node{
+			Key:    []byte("A"),
+			Bucket: true,
+		})
+		So(underNodes, ShouldContain, Node{
+			Key:    []byte("B"),
+			Bucket: true,
+		})
+		So(underNodes, ShouldContain, Node{
+			Key:   []byte("C"),
+			Value: []byte("kyle"),
+		})
 	})
+
+	Convey("Close should shutdown the Raft connection", t, func() {
+		err := d.Close()
+		So(err, ShouldBeNil)
+	})
+
+	Convey("Snapshots should be restartable from disk", t, func() {
+		d, _, tmpDir, _ := mustRaft("d1")
+		defer os.RemoveAll(tmpDir)
+		defer d.Close()
+
+		b, _ := d.Bucket("A")
+		b.Set(Node{
+			Key:   []byte("test"),
+			Value: []byte("test"),
+		})
+
+		sm := (*fsm)(d.(*Raft))
+		snap, err := sm.Snapshot()
+		So(err, ShouldBeNil)
+		snapFile, err := os.Create("snapfile")
+		defer os.Remove("snapfile")
+
+		sink := &mockSink{snapFile}
+		err = snap.Persist(sink)
+		So(err, ShouldBeNil)
+
+		backupFile, _ := os.Open("snapfile")
+		err = sm.Restore(backupFile)
+		So(err, ShouldBeNil)
+
+		r := (*Raft)(sm)
+		b2, _ := r.Bucket("A")
+		val, err := b2.Get([]byte("test"))
+		So(err, ShouldBeNil)
+		So(val, ShouldResemble, []byte("test"))
+	})
+
+	Convey("Multi-peer Stores work", t, func() {
+		_, _, tempDir1, err := mustRaft("d1")
+		defer os.RemoveAll(tempDir1)
+		So(err, ShouldBeNil)
+
+		_, _, tempDir2, err := mustRaft("d2")
+		defer os.RemoveAll(tempDir2)
+		So(err, ShouldBeNil)
+
+		_, _, tempDir3, err := mustRaft("d3")
+		defer os.RemoveAll(tempDir3)
+		So(err, ShouldBeNil)
+
+	})
+}
+
+func mustRaft(dir string) (Consensus, Store, string, error) {
+	tmpDir, _ := ioutil.TempDir("", "raft_test")
+
+	s, _ := NewDB(fmt.Sprintf("%s/raft_test.db", tmpDir))
+	d, err := NewRaft(tmpDir, "127.0.0.1:0", s)
+
+	return d, s, tmpDir, err
+}
+
+type mockSink struct {
+	*os.File
+}
+
+func (s *mockSink) ID() string {
+	return "1"
+}
+func (s *mockSink) Cancel() error {
+	return nil
 }
